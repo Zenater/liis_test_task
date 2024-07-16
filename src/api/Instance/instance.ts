2c146b631b09aa93cdfb56e34ba931c8d3d10b27
Vitaly Volkov <zent230096@mail.ru>
2024-07-16 20:01:44 +0300

    change favicon and name project. add description to readme

--- src/api/Instance/instance.ts
+++ src/api/Instance/instance.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const API_URL = process.env.REACT_APP_PUBLIC_URL;
+export const API_URL =
+  process.env.REACT_APP_PUBLIC_URL ||
+  'https://engine.hotellook.com/api/v2/cache.json';
 
 export const instance = axios.create({
   baseURL: API_URL,
